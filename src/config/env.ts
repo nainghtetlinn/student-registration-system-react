bd45784ed64eef701ccd48bb29ca9c0e93772d1a
nainghtetlinn <naing.work.it@gmail.com>
2025-07-21 21:39:35 +0630

    setup env, axios and paths

---
+++ src/config/env.ts
@@ -0,0 +1,35 @@
+import * as z from 'zod'
+
+const createEnv = () => {
+  const EnvSchema = z.object({
+    API_URL: z.string(),
+  })
+
+  const envVars = Object.entries(import.meta.env).reduce<
+    Record<string, string>
+  >((acc, curr) => {
+    const [key, value] = curr
+    if (key.startsWith('VITE_APP_')) {
+      acc[key.replace('VITE_APP_', '')] = value
+    }
+    return acc
+  }, {})
+
+  const parsedEnv = EnvSchema.safeParse(envVars)
+
+  if (!parsedEnv.success) {
+    console.log(z.treeifyError(parsedEnv.error))
+    throw new Error(
+      `Invalid env provided.
+The following variables are missing or invalid:
+${Object.entries(z.treeifyError(parsedEnv.error).properties ?? {})
+  .map(([k, v]) => `- ${k}: ${v.errors.join(' | ')}`)
+  .join('\n')}
+`,
+    )
+  }
+
+  return parsedEnv.data
+}
+
+export const env = createEnv()
---
+++ src/config/paths.ts
@@ -0,0 +1,15 @@
+export const paths = {
+  home: {
+    path: '/',
+    getHref: () => '/',
+  },
+  auth: {
+    login: {
+      path: '/auth/login',
+      getHref: (redirectTo?: string) =>
+        `/auth/login${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+    },
+  },
+
+  app: {},
+} as const
