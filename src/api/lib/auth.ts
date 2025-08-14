403fe08ee587cb17faba0f04feddaa39294e7edf
nainghtetlinn <naing.work.it@gmail.com>
2025-08-14 21:36:10 +0630

    feat: bind login api with backend and update response types

--- src/api/lib/auth.ts
+++ src/api/lib/auth.ts
@@ -1,7 +1,7 @@
-import { configureAuth } from 'react-query-auth'
 import { z } from 'zod'
+import { configureAuth } from 'react-query-auth'
 
-import type { AuthResponse } from '@/types/api'
+import type { ApiResponse, LoginResponse } from '@/types/api'
 import { api } from './axios'
 
 export const loginInputSchema = z.object({
@@ -13,39 +13,26 @@ export type TLoginInput = z.infer<typeof loginInputSchema>
 
 export const loginWithEmailAndPassword = (
   data: TLoginInput,
-): Promise<AuthResponse> => {
-  return api.post('/auth/login', data)
+): Promise<ApiResponse<LoginResponse>> => {
+  return api.post('/auth/users/login', data)
 }
 
-export const registerInputSchema = z.object({
-  email: z.email(),
-  password: z.string().min(6),
+export const { useUser, useLogin, useRegister, useLogout } = configureAuth({
+  userFn: () => {
+    console.log('Method not implemented yet.')
+    return {} as LoginResponse
+  },
+  loginFn: async (data: TLoginInput) => {
+    const response = await loginWithEmailAndPassword(data)
+    return response.data
+  },
+  registerFn: async () => {
+    console.log('Method not implemented yet.')
+    return {} as LoginResponse
+  },
+  logoutFn: async () => {
+    console.log('Method not implemented yet.')
+    return
+  },
+  userKey: ['user'],
 })
-
-export type TRegisterInput = z.infer<typeof registerInputSchema>
-
-export const registerWithEmailAndPassword = (
-  data: TRegisterInput,
-): Promise<AuthResponse> => {
-  return api.post('/auth/register', data)
-}
-
-export const { useUser, useLogin, useLogout, useRegister, AuthLoader } =
-  configureAuth({
-    userFn: async () => {
-      const response = await api.get('/auth/me')
-      return response.data
-    },
-    loginFn: async (data: TLoginInput) => {
-      const response = await loginWithEmailAndPassword(data)
-      return response.user
-    },
-    registerFn: async (data: TRegisterInput) => {
-      const response = await registerWithEmailAndPassword(data)
-      return response.user
-    },
-    logoutFn: async () => {
-      const response = await api.post('/auth/logout')
-      return response.data
-    },
-  })
