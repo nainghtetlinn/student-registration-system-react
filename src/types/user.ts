d61e8edcdcfb0696f2abb6d25cc443e29ee8586d
nainghtetlinn <naing.work.it@gmail.com>
2025-08-19 19:17:18 +0630

    feat: update user type to include createdAt and updatedAd fields; add response types for change password, verify OTP, and reset password

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,1 +1,7 @@
-export type TUser = { name: string | null; email: string; role: string }
+export type TUser = {
+  name: string | null
+  email: string
+  role: string
+  createdAt: Date
+  updatedAd: Date
+}
