7527b0874b986438a44776d4c8c7d526ce0b2928
nainghtetlinn <naing.work.it@gmail.com>
2025-08-19 19:21:27 +0630

    feat: implement change password form with validation and submission handling

---
+++ src/features/auth/components/change-password-form.tsx
@@ -0,0 +1,75 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Form } from '@/components/ui/form'
+import { FormInputField } from '@/components/ui/form-fields'
+import { Loader2 } from 'lucide-react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import {
+  changePasswordInputSchema,
+  useChangePassword,
+  type TChangePasswordInput,
+} from '@/api/lib/auth'
+
+export const ChangePasswordForm = ({
+  onSuccess,
+}: {
+  onSuccess: () => void
+}) => {
+  const form = useForm({
+    resolver: zodResolver(changePasswordInputSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  const { mutate, isPending } = useChangePassword({
+    onSuccess,
+    onError: (error) => {
+      if (error instanceof AxiosError)
+        toast.error(error.response?.data?.message || error.message)
+    },
+  })
+
+  const onSubmit = (data: TChangePasswordInput) => {
+    mutate(data)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='w-full max-w-xl'
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Change Password</CardTitle>
+          </CardHeader>
+          <CardContent className='grid gap-4'>
+            <FormInputField
+              control={form.control}
+              name='email'
+              label='Email'
+              placeholder='example@gmail.com'
+            />
+          </CardContent>
+          <CardFooter className='flex justify-between'>
+            <Button disabled={isPending}>
+              Change {isPending && <Loader2 className='animate-spin' />}
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  )
+}
