411bb8d6c847de86f2983a8bdff639e71cb51345
nainghtetlinn <naing.work.it@gmail.com>
2025-08-23 21:19:16 +0630

    refactor: move useGetAllAccounts hook and schema to API layer

--- src/api/admin/get-all-accounts.ts
+++ src/api/admin/get-all-accounts.ts
@@ -1,6 +1,26 @@
-import type { TFilterGetAccountsInput } from '@/features/admin/utils/schemas'
-import { api } from '../lib/axios'
+import {
+  useInfiniteQuery,
+  type QueryKey,
+  type UseInfiniteQueryOptions,
+} from '@tanstack/react-query'
+import { z } from 'zod'
+
 import type { ApiResponse, GetAllAccountsResponse } from '@/types/api'
+import type { TUser } from '@/types/user'
+import { api } from '../lib/axios'
+
+export const filterGetAccountsInputSchema = z.object({
+  keyword: z.string().optional(),
+  role: z.string().optional(),
+  page: z.coerce.number().min(0).optional(),
+  size: z.coerce.number().min(1).optional(),
+  sortField: z.string().optional(),
+  sortDirection: z.string().optional(),
+})
+
+export type TFilterGetAccountsInput = z.infer<
+  typeof filterGetAccountsInputSchema
+>
 
 export const getAllAccounts = (search: TFilterGetAccountsInput) => {
   const params = {
@@ -10,5 +30,34 @@ export const getAllAccounts = (search: TFilterGetAccountsInput) => {
   }
   return api.get<ApiResponse<GetAllAccountsResponse>>('/admin/getAllAccounts', {
     params,
+  })
+}
+
+type UsersPage = {
+  users: TUser[]
+  nextPage: number | null
+}
+
+export const useGetAllAccounts = (
+  options: Omit<
+    UseInfiniteQueryOptions<UsersPage, Error, TUser[], QueryKey, number>,
+    'queryFn' | 'initialPageParam' | 'getNextPageParam'
+  >,
+  search?: Omit<TFilterGetAccountsInput, 'page'>,
+) => {
+  return useInfiniteQuery({
+    queryFn: async ({ pageParam }) => {
+      const response = await getAllAccounts({ page: pageParam, ...search })
+      const { currentPage, totalPages } = response.data.meta
+      const current = currentPage - 1
+      return {
+        users: response.data.data,
+        nextPage: current < totalPages ? current + 1 : null,
+      }
+    },
+    select: (data) => data.pages.flatMap((p) => p.users),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    ...options,
   })
 }
