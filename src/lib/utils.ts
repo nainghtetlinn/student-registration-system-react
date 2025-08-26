d1e2de2483718d6fd0db3c3cb5904d47361885ee
nainghtetlinn <naing.work.it@gmail.com>
2025-08-26 20:41:30 +0630

    feat: add nrcStringToObject utility function to parse NRC strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,3 +24,26 @@ export function nrcObjectToString(nrc: TNrcSchema): string {
 
   return `${transformedNrc.stateCode}/${transformedNrc.townshipCode}(${transformedNrc.nrcType})${transformedNrc.nrcNumber}`
 }
+
+export function nrcStringToObject(nrcString: string): TNrcSchema | null {
+  const nrcPattern =
+    /^([1-9]|1[0-3])\/([A-Za-z]{1,6})\((N|E|P|T|Y|S)\)([0-9၀၁၂၃၄၅၆၇၈၉]{6})$/
+  const match = nrcString.match(nrcPattern)
+
+  if (!match) return null
+
+  const stateCode = nrcStates.find((v) => v.number.en === match[1])?.id || ''
+  const townshipCode =
+    nrcTownships.find((v) => v.short.en === match[2])?.id || ''
+  const nrcType = nrcTypes.find((v) => v.name.en === match[3])?.id || ''
+  const nrcNumber = match[4]
+
+  if (!stateCode || !townshipCode || !nrcType) return null
+
+  return {
+    stateCode,
+    townshipCode,
+    nrcType,
+    nrcNumber,
+  }
+}
