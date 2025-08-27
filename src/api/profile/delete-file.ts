4393dbf0ede3df34437e81560f542e01cb6321df
nainghtetlinn <naing.work.it@gmail.com>
2025-08-27 22:11:36 +0630

    feat: Add profile photo deletion API hook

---
+++ src/api/profile/delete-file.ts
@@ -0,0 +1,49 @@
+import {
+  useMutation,
+  useQueryClient,
+  type UseMutationOptions,
+} from '@tanstack/react-query'
+import { api } from '../lib/axios'
+import { toast } from 'sonner'
+import type { TProfile } from '@/types/profile'
+import { AxiosError } from 'axios'
+import type { ApiResponse } from '@/types/api'
+
+const deleteFile = (type: 'Profile Photo' | 'Signature') => {
+  return api.delete<ApiResponse<string>>('/staff/profile/deleteFile', {
+    params: { type },
+  })
+}
+
+export const useDeleteProfileFile = (
+  options?: Omit<
+    UseMutationOptions<string, Error, unknown>,
+    'mutationKey' | 'mutationFn'
+  >,
+) => {
+  const queryClient = useQueryClient()
+
+  const { onSuccess, onError, ...restOptions } = options ?? {}
+
+  return useMutation({
+    mutationKey: ['delete-file', 'profile'],
+    mutationFn: async () => {
+      const response = await deleteFile('Profile Photo')
+      return response.data.message
+    },
+    onSuccess: (response, ...restArgs) => {
+      toast.success(response)
+      queryClient.setQueryData(['profile'], (old: TProfile) => ({
+        ...old,
+        photoUrl: null,
+      }))
+      onSuccess?.(response, ...restArgs)
+    },
+    onError: (error, ...restArgs) => {
+      if (error instanceof AxiosError)
+        toast.error(error.response?.data?.message || error.message)
+      onError?.(error, ...restArgs)
+    },
+    ...restOptions,
+  })
+}
