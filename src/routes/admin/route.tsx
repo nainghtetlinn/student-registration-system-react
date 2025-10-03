c848544025128058aef9bf1f115de1cecf5e0e53
nainghtetlinn <naing.work.it@gmail.com>
2025-10-03 12:52:53 +0630

    feat: Add profile creation redirection logic in admin route

--- src/routes/admin/route.tsx
+++ src/routes/admin/route.tsx
@@ -1,4 +1,5 @@
 import { getUserQuery } from '@/api/lib/auth'
+import { getProfileQuery } from '@/api/profile/get-profile'
 import { AppHeader } from '@/components/layouts/admin/AppHeader'
 import { AppSidebar } from '@/components/layouts/admin/AppSidebar'
 import { Pending } from '@/components/layouts/shared/pending'
@@ -27,12 +28,44 @@ export const Route = createFileRoute('/admin')({
     if (!user) {
       shouldRedirect = true
     } else if (user.updatedAt == null) {
+      // user is logged in but haven't changed his password
       throw redirect({
         to: paths.auth.changePassword.getHref(),
         search: {
           redirect: location.href,
           email: user.email,
         },
+      })
+    }
+
+    let shouldProfileCreate = false
+    try {
+      const profile =
+        await context.queryClient.ensureQueryData(getProfileQuery())
+      if (!profile) shouldProfileCreate = true
+    } catch (error) {
+      console.log(error)
+      shouldProfileCreate = true
+    }
+
+    if (
+      shouldProfileCreate &&
+      !location.href.includes(paths.admin.profile.create.getHref())
+    ) {
+      // redirect profile/create if profile is undefined
+      throw redirect({
+        to: paths.admin.profile.create.getHref(),
+        search: {
+          redirect: location.href,
+        },
+      })
+    } else if (
+      !shouldProfileCreate &&
+      location.href.includes(paths.admin.profile.create.getHref())
+    ) {
+      // redirect if profile created and trying to go to profile/create route
+      throw redirect({
+        to: paths.admin.profile.root.getHref(),
       })
     }
 
