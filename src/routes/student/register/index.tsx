b243042bd5617010ca781ef26ccb8ecb0cd24a5e
nainghtetlinn <naing.work.it@gmail.com>
2025-10-08 12:31:38 +0630

    redirect if already registered entrance form

--- src/routes/student/register/index.tsx
+++ src/routes/student/register/index.tsx
@@ -3,14 +3,25 @@ import { EntranceForm } from '@/features/student/components/entrance-form'
 import { createFileRoute, redirect, useRouter } from '@tanstack/react-router'
 
 import { useCreateEntranceForm } from '@/api/student/create-entrance-form'
+import { getEntranceFormQuery } from '@/api/student/get-entrance-form'
 import { paths } from '@/config/paths'
 import { entranceFormDefaults } from '@/features/student/schemas/entrance-form-schema'
 
 export const Route = createFileRoute('/student/register/')({
   component: RouteComponent,
-  beforeLoad: ({ context }) => {
-    const form = context.queryClient.getQueryData(['entrance form'])
-    if (form)
+  beforeLoad: async ({ context }) => {
+    let shouldRedirect = false
+
+    try {
+      const form = await context.queryClient.ensureQueryData(
+        getEntranceFormQuery(),
+      )
+      if (form) shouldRedirect = true
+    } catch (error) {
+      console.log(error)
+    }
+
+    if (shouldRedirect)
       throw redirect({
         to: paths.home.getHref(),
       })
