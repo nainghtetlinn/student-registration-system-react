ca0b8064af5630ecff53fc49ec8858a75b93d508
nainghtetlinn <naing.work.it@gmail.com>
2025-10-17 20:42:32 +0630

    feat: file upload api

---
+++ src/features/student/forms/entrance/api/upload-photo.api.ts
@@ -0,0 +1,79 @@
+import type { ApiResponse } from '@/types/api'
+import type { TForm } from '@/types/form'
+import type { UseMutationOptions } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
+import type { TEntranceFormSchema } from '../schema/entrance-form.schema'
+import type { TFiles } from '../types/get.type'
+
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { api } from '@/api/lib/axios'
+
+const MAX_UPLOAD_SIZE = 1024 * 1024 * 1 // 1MB
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/jpg',
+  'image/webp',
+]
+
+export const imageSchema = z
+  .instanceof(File)
+  .refine(
+    (file) => file.size <= MAX_UPLOAD_SIZE,
+    `Max image size is ${MAX_UPLOAD_SIZE / (1024 * 1024)}MB.`,
+  )
+  .refine(
+    (file) => ACCEPTED_IMAGE_TYPES.includes(file.type),
+    'Only .jpg, .jpeg, .png and .webp formats are accepted.',
+  )
+
+const uploadPhoto = (file: File) => {
+  const formData = new FormData()
+  formData.append('file', file)
+
+  return api.patch<ApiResponse<string>>(
+    '/student/entranceForm/uploadPhoto',
+    formData,
+  )
+}
+
+export const useUploadPhoto = (
+  options?: Omit<
+    UseMutationOptions<string, AxiosError<ApiResponse<string>>, File>,
+    'mutationKey' | 'mutationFn'
+  >,
+) => {
+  const queryClient = useQueryClient()
+
+  const { onSuccess, onError, ...restOptions } = options ?? {}
+
+  return useMutation({
+    mutationKey: ['form', 'entrance', 'upload-photo'],
+    mutationFn: async (data) => {
+      const response = await uploadPhoto(data)
+      return response.data.data
+    },
+    onSuccess: (response, ...restArgs) => {
+      toast.success('Photo uploaded successfully')
+      queryClient.setQueryData(
+        ['form', 'entrance'],
+        (old: {
+          formDetails: TForm
+          formData: TEntranceFormSchema
+          files: TFiles
+        }) => {
+          return { ...old, files: { ...old.files, studentPhotoUrl: response } }
+        },
+      )
+      onSuccess?.(response, ...restArgs)
+    },
+    onError: (error, ...restArgs) => {
+      toast.error(error.response?.data?.message || error.message)
+      onError?.(error, ...restArgs)
+    },
+    ...restOptions,
+  })
+}
