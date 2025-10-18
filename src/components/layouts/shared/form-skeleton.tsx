1dd82df76777ad73a458bd4b2c629bbdef169dca
nainghtetlinn <naing.work.it@gmail.com>
2025-10-18 18:51:08 +0630

    fix: update form skeleton height

--- src/components/layouts/shared/form-skeleton.tsx
+++ src/components/layouts/shared/form-skeleton.tsx
@@ -2,6 +2,6 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export const FormSkeleton = () => {
   return (
-    <Skeleton className='relative container mx-auto mt-4 h-[500px] max-w-3xl rounded' />
+    <Skeleton className='relative container mx-auto mt-4 h-[800px] max-w-3xl rounded' />
   )
 }
