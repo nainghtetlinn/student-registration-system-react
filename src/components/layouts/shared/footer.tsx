45cfb292c3402c37047f6d041677244bc4303152
nainghtetlinn <naing.work.it@gmail.com>
2025-10-24 18:27:06 +0630

    ui: add mt to footer

--- src/components/layouts/shared/footer.tsx
+++ src/components/layouts/shared/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className='bg-accent text-accent-foreground flex h-[200px] items-center justify-center'>
+    <footer className='bg-accent text-accent-foreground mt-4 flex h-[200px] items-center justify-center'>
       <p>© 2025 Technological University Taunggyi. All rights reserved.</p>
     </footer>
   )
